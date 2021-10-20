d4a6668310e1cc5355ef793db32ebf2bb878050e
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2021-10-20 14:55:12 +0100

    Car acceleration and rotation working

---
+++ Assets/Scripts/SimpleCarController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCarController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float verticalInput;
+    private float steeringAngle;
+
+    public WheelCollider frontLeftW, frontRightW;
+    public WheelCollider rearLeftW, rearRightW;
+    public Transform frontLeftT, frontRightT;
+    public Transform rearLeftT, rearRightT;
+    public float maxSteerAngle = 30;
+    public float motorForce = 50;
+
+    public void GetInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void Steer()
+    {
+        steeringAngle = maxSteerAngle * horizontalInput;
+        frontLeftW.steerAngle = steeringAngle;
+        frontRightW.steerAngle = steeringAngle;
+    }
+
+    private void Accelerate()
+    {
+        frontLeftW.motorTorque = verticalInput * motorForce;
+        frontRightW.motorTorque = verticalInput * motorForce;
+    }
+
+    private void UpdateWheelPoses()
+    {
+        UpdateWheelPose(frontLeftW, frontLeftT);
+        UpdateWheelPose(frontRightW, frontRightT);
+        UpdateWheelPose(rearLeftW, rearLeftT);
+        UpdateWheelPose(rearRightW, rearRightT); 
+    }
+
+    private void UpdateWheelPose(WheelCollider _collider, Transform _transform)
+    {
+        Vector3 _pos = _transform.position;
+        Quaternion _quat = _transform.rotation;
+
+        _collider.GetWorldPose(out _pos, out _quat);
+
+        _transform.position = _pos;
+        _transform.rotation = _quat;
+    }
+
+    private void FixedUpdate()
+    {
+        GetInput();
+        Steer();
+        Accelerate();
+        UpdateWheelPoses();
+    }
+}
